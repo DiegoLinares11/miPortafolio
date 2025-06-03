e5d3e3099f6c1333af63a57aeb8308d575c48763
DiegoLinares11 <lin221256@uvg.edu.gt>
2025-06-02 21:21:04 -0600

    Structure of the about page and css of reference

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section id="about">
+      <h2>Sobre mí</h2>
+      <p>Me apasiona el desarrollo web y aprender nuevas tecnologías para crear experiencias únicas.</p>
+    </section>
+  );
+}
